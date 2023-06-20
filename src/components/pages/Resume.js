01b8d69f35c9df64af986a9bc80bccbac40e710c
cristino4 <117871733+cristino4@users.noreply.github.com>
2023-06-19 23:10:52 -0700

    updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -7,7 +7,7 @@ const Resume = () => {
 
     return (
         <div className="my-10 mx-auto">
-            <a href="./20221128_Cristino_Castro_Resume_I.pdf" className="text-2xl font-medium font-sans text-gray-500 border rounded-full p-2">Download Resume</a>
+            <a href="./20230614_Cristino_Castro_Resume_J.pdf" className="text-2xl font-medium font-sans text-gray-500 border rounded-full p-2">Download Resume</a>
         </div>
     );
 };
