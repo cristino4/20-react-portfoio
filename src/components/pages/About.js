5b2d279db59496fe31a348f8d0d3cc2475eadc3f
cristino4 <117871733+cristino4@users.noreply.github.com>
2024-02-06 23:10:48 -0800

    updated resume links and about me text

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -12,8 +12,8 @@ const About = ({handlePageChange}) => {
                     <img src="./rocket.jpg" className="max-w-sm rounded-lg shadow-2xl" alt='me and my sister' />
                     <div>
                         <p className="font-sans text-5xl font-bold">Welcome!<br></br><br></br>I'm Cristino Castro.</p>
-                        <p className="font-sans py-6">I'm an electrical engineer, software developer, and musician from 
-                        Hesperia, California (located just outside of Los Angeles). <br></br><br></br>
+                        <p className="font-sans py-6">I'm an electrical engineer and software developer from 
+                        Anaheim, California. <br></br><br></br>
                         I was raised in a small family with my only sibling and sister Caroline (seen with me on the left). 
                         Since I was a kid, my sister and I have been facinated by science and engineering. I remember 
                         bindge watching Bill Nye (the science guy, remember?) and PBS NOVA shows on TV or cassette. In school I was thrilled at 
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -7,7 +7,7 @@ const Resume = () => {
 
     return (
         <div className="my-10 mx-auto">
-            <a href="./20230614_Cristino_Castro_Resume_J.pdf" className="text-2xl font-medium font-sans text-gray-500 border rounded-full p-2">Download Resume</a>
+            <a href="./20240205_Cristino_Castro_Resume_M.pdf" className="text-2xl font-medium font-sans text-gray-500 border rounded-full p-2">Download Resume</a>
         </div>
     );
 };
